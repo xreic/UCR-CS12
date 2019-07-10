c5d83b228cfd886186703d19c2711c2418d12ed2
xreic <32492656+xreic@users.noreply.github.com>
2019-07-09 18:28:33 -0700

    Update main.cpp

--- Lab 11-14/main.cpp
+++ Lab 11-14/main.cpp
@@ -3,15 +3,15 @@
 int main(){
 	//tests constructor, destructor, push_front, pop_front, display
 
-   {
-    cout << "\nlist1 constructor called";
-    IntList list1;
+	{
+	cout << "\nlist1 constructor called";
+	IntList list1;
     cout << "\npushfront 10";
     list1.push_front(10);
     cout << "\npushfront 20";
     list1.push_front(20);
     cout << "\npushfront 30";
-    list1.push_front(30);
+	list1.push_front(30);
     cout << "\nlist1: ";
     list1.display();
     cout << "\npop";
@@ -27,10 +27,10 @@ int main(){
     cout << "\nlist1: ";
     list1.display();
     cout << endl;
-   }
+   	}
 	
-   cout << "list1 destructor called" << endl;
+	cout << "list1 destructor called" << endl;
 
-   return 0;
+	return 0;
 }
 	
