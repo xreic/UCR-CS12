9c963f44c103d86ed3a21834c59a7d1ddd4e4fcc
xreic <32492656+xreic@users.noreply.github.com>
2019-07-09 18:29:43 -0700

    Update main.cpp

--- Lab 11-14/main.cpp
+++ Lab 11-14/main.cpp
@@ -6,27 +6,27 @@ int main(){
 	{
 	cout << "\nlist1 constructor called";
 	IntList list1;
-    cout << "\npushfront 10";
-    list1.push_front(10);
-    cout << "\npushfront 20";
-    list1.push_front(20);
-    cout << "\npushfront 30";
+	cout << "\npushfront 10";
+	list1.push_front(10);
+	cout << "\npushfront 20";
+	list1.push_front(20);
+	cout << "\npushfront 30";
 	list1.push_front(30);
-    cout << "\nlist1: ";
-    list1.display();
-    cout << "\npop";
-    list1.pop_front();
-    cout << "\nlist1: ";
-    list1.display();
-    cout << "\npop";
-    list1.pop_front();
-    cout << "\nlist1: ";
-    list1.display();
-    cout << "\npop";
+	cout << "\nlist1: ";
+	list1.display();
+	cout << "\npop";
 	list1.pop_front();
-    cout << "\nlist1: ";
-    list1.display();
-    cout << endl;
+	cout << "\nlist1: ";
+	list1.display();
+	cout << "\npop";
+	list1.pop_front();
+	cout << "\nlist1: ";
+	list1.display();
+	cout << "\npop";
+	list1.pop_front();
+	cout << "\nlist1: ";
+	list1.display();
+	cout << endl;
    	}
 	
 	cout << "list1 destructor called" << endl;
