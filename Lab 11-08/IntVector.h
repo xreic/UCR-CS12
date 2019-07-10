cd80aa804dd78fd45fc93e63ee390ae146ae7377
xreic <32492656+xreic@users.noreply.github.com>
2019-07-09 18:22:55 -0700

    Update IntVector.h

--- Lab 11-08/IntVector.h
+++ Lab 11-08/IntVector.h
@@ -20,12 +20,17 @@ class IntVector{
 		unsigned capacity() const;
 		bool empty() const;
 		const int& at(unsigned index) const;
+  
 // 		int& at(unsigned index);
 // 		void insert(unsigned index, int value);
 // 		void erase(unsigned index);
+  
 		const int& front() const;
+  
 // 		int& front();
+  
 		const int& back() const;
+  
 // 		int& back();
 // 		void assign(unsigned n, int value);
 // 		void push_back(int value);
@@ -35,8 +40,10 @@ class IntVector{
 // 		void reserve(unsigned n);
 		
 // 		void list() const;
+  
 // 	private:
 // 		void expand();
 // 		void expand(unsigned amount); 
+  
 };
 #endif
