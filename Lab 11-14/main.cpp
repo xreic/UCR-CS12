86c4dcffe589ce1c7e08c30c1a1ab02770a41f37
xreic <32492656+xreic@users.noreply.github.com>
2019-07-09 18:25:58 -0700

    Create main.cpp

---
+++ Lab 11-14/main.cpp
@@ -0,0 +1,35 @@
+#include "IntList.h"
+
+int main(){
+	//tests constructor, destructor, push_front, pop_front, display
+
+   {
+    cout << "\nlist1 constructor called";
+    IntList list1;
+    cout << "\npushfront 10";
+    list1.push_front(10);
+    cout << "\npushfront 20";
+    list1.push_front(20);
+    cout << "\npushfront 30";
+    list1.push_front(30);
+    cout << "\nlist1: ";
+    list1.display();
+    cout << "\npop";
+    list1.pop_front();
+    cout << "\nlist1: ";
+    list1.display();
+    cout << "\npop";
+    list1.pop_front();
+    cout << "\nlist1: ";
+    list1.display();
+    cout << "\npop";
+	list1.pop_front();
+    cout << "\nlist1: ";
+    list1.display();
+    cout << endl;
+   }
+   cout << "list1 destructor called" << endl;
+
+   return 0;
+}
+	
